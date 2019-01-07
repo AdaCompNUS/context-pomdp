19f7a07ba8b56c69993055b4db70b44c2550658a
cai-panpan <pcai2@e.ntu.edu.sg>
2019-01-07 21:29:35 +0800

    Optimal action with both lowerbound and visite count

--- car_hyp_despot/src/planner/Path.cpp
+++ car_hyp_despot/src/planner/Path.cpp
@@ -127,8 +127,8 @@ double Path::getlength(){
 double Path::getCurDir(){
     auto& path = *this;
 
-    if (path.size() <= 1)
+    if (path.size() <= 100)
     	return 0.0;
 
-	return COORD::SlopAngle(path[0], path[3]);
+	return COORD::SlopAngle(path[0], path[100]);
 }
