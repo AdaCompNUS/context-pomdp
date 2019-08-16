61fb6b914730c4de6fd41596bca7eb164d157d7b
leeyiyuan <leeyiyuan@hotmail.com>
2019-08-16 22:12:34 +0800

    Improve rendering logic

--- carla_connector2/src/spectator.py
+++ carla_connector2/src/spectator.py
@@ -16,7 +16,6 @@ class Spectator(Drunc):
 
         self.actor = None
         self.camera_sensor_actor = None
-        self.camera_bitmap = None
 
         self.world_tick_callback_id = self.world.on_tick(self.world_tick_callback)
 
@@ -27,7 +26,6 @@ class Spectator(Drunc):
             else:
                 self.actor = None
                 self.camera_sensor_actor = None
-                self.camera_bitmap = None
         
         for actor in self.world.get_actors():
             if actor.is_alive and actor.attributes.get('role_name') == 'ego_vehicle':
@@ -37,27 +35,18 @@ class Spectator(Drunc):
         if self.actor is not None:
             camera_blueprint = random.choice(self.world.get_blueprint_library().filter('sensor.camera.rgb'))
             # TODO Add as ROS parameters.
-            camera_blueprint.set_attribute('image_size_x', '800')
-            camera_blueprint.set_attribute('image_size_y', '600')
+            camera_blueprint.set_attribute('image_size_x', '1920')
+            camera_blueprint.set_attribute('image_size_y', '1080')
             self.camera_sensor_actor = self.world.spawn_actor(
                 camera_blueprint,
-                carla.Transform(carla.Location(x=-16.0, z=12.0), carla.Rotation(pitch=-20.0)),
+                carla.Transform(carla.Location(x=-32.0, z=24.0), carla.Rotation(pitch=-30.0)),
                 attach_to=self.actor)
             self.camera_sensor_actor.listen(self.camera_image_callback)
 
     def camera_image_callback(self, image):
-        if self.actor is None or not self.actor.is_alive:
-            return
-
-        image.convert(carla.ColorConverter.Raw)
         array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
         array = np.reshape(array, (image.height, image.width, 4))
-        array = array[:, :, :3]
-        self.camera_bitmap = array
-    
-    def update(self):
-        if self.camera_bitmap is not None:
-            cv2.imshow('spectator', self.camera_bitmap)
+        cv2.imshow('spectator', array)
 
 if __name__ == '__main__':
     rospy.init_node('spectator')
@@ -67,9 +56,6 @@ if __name__ == '__main__':
     cv2.startWindowThread()
     cv2.namedWindow('spectator')
 
-    rate = rospy.Rate(60)
-    while not rospy.is_shutdown():
-        spectator.update()
-        rate.sleep()
+    rospy.spin()
 
     cv2.destroyWindow('spectator')
