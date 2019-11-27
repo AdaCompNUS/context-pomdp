39b05e4f466a2956dfe0546e3f47029ee70ad548
panpan <pcai2@e.ntu.edu.sg>
2019-11-27 17:35:07 +0800

    add new visualization script

---
+++ summit_connector/src/visualize_live.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+
+'''
+
+[Road Lanes]   [Sidewalk Lanes]       [Obstacles]
+
+[Road Agents]  [Sidewalk Agents]  
+
+[Ego Path]
+
+[Ego Agent]
+
+'''
+
+from summit_dql import SummitDQL
+import carla
+import rospy
+import msg_builder.msg
+import cv2
+import time
+import std_msgs.msg
+
+class VisualizeLive(SummitDQL):
+    def __init__(self):
+        super(VisualizeLive, self).__init__()
+
+    def update(self):
+        cv2.imshow('frame', self.draw_state_frame())
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+              return False
+
+        return True
+
+if __name__ == '__main__':
+    rospy.init_node('visualize_live')
+    rospy.wait_for_message("/meshes_spawned", std_msgs.msg.Bool)
+    rospy.wait_for_message("/ego_state", msg_builder.msg.car_info)
+    visualize_live = VisualizeLive()
+
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        if not visualize_live.update():
+            break
+        rate.sleep()
+        end = time.time()
