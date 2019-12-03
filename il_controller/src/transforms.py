20561c2d5be09870420a6875487cdfd72d8d65b0
panpan <pcai2@e.ntu.edu.sg>
2019-12-03 15:06:07 +0800

    used limits when converting normalized steering to onehot encoding

--- il_controller/src/transforms.py
+++ il_controller/src/transforms.py
@@ -156,12 +156,13 @@ def make_onehot(num_bins, bin_idx, prob):
 
 def float_to_onehot(v, v_min, v_max, num_bins):
     try:
-        if v > v_max - 0.0001:
+        if v > v_max:
             print("float_to_onehot warning: v > v_max, v={}".format(v), flush=True)
             v = v_max - 0.0001
         elif v < v_min:
             print("float_to_onehot warning: v < v_min, v={}".format(v), flush=True)
             v = v_min
+        v = min(v, v_max - 0.0001)
         onehot_resolution = (v_max - v_min) / float(num_bins)
         bin_idx = int(np.floor(((v - v_min) / onehot_resolution)))
 
